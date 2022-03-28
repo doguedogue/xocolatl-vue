f11ad4551b466a6c7e502919a2386e83508ef8ef
Rafael Vilches <rvilches38@yahoo.com.mx>
2022-03-27 20:11:01 -0600

    vuex state template for modules

---
+++ src/store/module-template/index.js
@@ -0,0 +1,13 @@
+
+import state from './state'
+import * as actions from './actions'
+import * as getters from './getters'
+import * as mutations from './mutations'
+
+const myCustomModule = {
+	namespaced: true,
+	actions,
+	getters,
+	mutations,
+	state
+}
---
+++ src/store/module-template/mutations.js
@@ -0,0 +1,4 @@
+
+// export const myMutations = async = ({ commit }) => {
+
+// }
